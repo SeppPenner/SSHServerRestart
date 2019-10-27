93142960ac41ba803ff5d5f7920d1e57d4955d7c
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:05:18 +0100

    Updated nuget packages, added GitVersionTask.

--- SSHServerRestart/Properties/AssemblyInfo.cs
+++ SSHServerRestart/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("89276c83-1cdb-4448-a5a6-0595cef94fe7")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
